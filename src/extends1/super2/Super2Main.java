2eaa180b97ecb5e36da494311aa9b4faa21a9193
Pangpanghoon <lostter0813@gmail.com>
2025-03-27 14:34:50 +0900

    추가

--- src/extends1/super2/Super2Main.java
+++ src/extends1/super2/Super2Main.java
@@ -3,7 +3,8 @@ package extends1.super2;
 public class Super2Main {
 
     public static void main(String[] args) {
-        ClassC classC = new ClassC();// 김광훈
+//        ClassC classC = new ClassC();
+        ClassB classB = new ClassB(100);
 
     }
 }
